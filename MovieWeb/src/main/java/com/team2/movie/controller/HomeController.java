ce4fe19aa6358d4f520156cfed2ccb7b96ded48e
bitcamp <bitcamp@DESKTOP-3IR1A2B>
2020-08-19 14:13:20 +0900

    testtetteess

--- MovieWeb/src/main/java/com/team2/movie/controller/HomeController.java
+++ MovieWeb/src/main/java/com/team2/movie/controller/HomeController.java
@@ -1,5 +1,12 @@
 package com.team2.movie.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class HomeController {
-
+	@GetMapping("/")
+	public String hi() {
+		return "main";
+	}
+	
 }
