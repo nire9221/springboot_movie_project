c3a4602087f5f1c60cded96a62d49de1e1337524
bitcamp <bitcamp@DESKTOP-OP2PV5C>
2020-08-19 11:45:49 +0900

    test

---
+++ MovieWeb/src/main/java/com/team3/movie/controller/HomeController.java
@@ -0,0 +1,5 @@
+package com.team3.movie.controller;
+
+public class HomeController {
+
+}
---
+++ MovieWeb/src/test/java/com/team3/movie/MovieWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
